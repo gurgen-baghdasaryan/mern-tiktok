c0a212b8b4e09ea6e005edd18695a302c6a7aa69
gugohh <oohhgug@gmail.com>
2022-02-03 16:27:56 +0100

    solved the import  bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react';
-import './App.css';
-import BarChart from "./components/BarChart"
-import {UserData}from './Data'
+import { useState } from "react";
+import "./App.css";
+import BarChart from "./components/BarChart";
+import {UserData} from "./Data"
+
 
 function App() {
-  const [UserData, setUserData] = useState({
-    labels:,
-    datasets:[]
-  })
 
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor:["green", "blue"],
+      },
+    ],
+  });
 
   return (
     <div className="App">
-      
-    <BarChart chartData={}/>
+      <div style={{ width: 700 }}>
+        <BarChart chartData={userData} />
+      </div>
     </div>
   );
 }
